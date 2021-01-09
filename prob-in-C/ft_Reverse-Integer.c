ee7504a8318257a4b5866bab152644ea1e07984b
vmakar0v <ahmedmontassir0@gmail.com>
2021-01-09 22:18:47 +0100

    32bit-ft_R.I

--- prob-in-C/ft_Reverse-Integer.c
+++ prob-in-C/ft_Reverse-Integer.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int reverse(int x){
-    int index = 0;
-    while (x != '\0')
-    {
-        index = index*10 + x%10;
+    long index= 0;
+    while (x != 0){
+        index = index * 10;  
+        index = index + x % 10;
         x = x / 10;
+    }
+    if(index > -2147483648 && index < 2147483647){
+        index=index;
+    }
+    else{
+        index = 0;
     }
     return index;
 }
-
-
 int main(){
-    int y;
+    long y;
     printf("type a num=\n");
-    scanf("%d", &y);
- 
-    int ret = reverse(y);
+    //scanf("%d", &y);
+    y = 2147483648;
+    long ret = reverse(y);
     printf("rv = %d \n", ret);
     return 0;
 }
