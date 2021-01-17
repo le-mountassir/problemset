5f30d4ad53206ed9306e17dfdb87035bbf8f317e
vmakar0v <ahmedmontassir0@gmail.com>
2021-01-17 01:28:04 +0100

    return

--- prob-in-C/persistence.c
+++ prob-in-C/persistence.c
@@ -1,36 +1,16 @@
 #include <stdio.h>
 int persistence(int n){
-    int k=n,j=0;
-    if(k < 10){
-        k=0;
-        return k; 
+  int j=0, i=1;
+  if(n<10){
+    return 0;    
+  }
+  else{
+    while(n>0){      
+      i*=(n%10);
+      n/=10;
     }
-    else{
-        while(k >=10){ 
-          if(n>=100000){
-              n=(n/100000)*((n%1000)/100)*(n%10)*((n%100)/10)*((n%10000)/1000)*((n%100000)/10000);
-              k=n;
-          }
-          else if(n>=10000){
-              n=(n/10000)*((n%1000)/100)*(n%10)*((n%100)/10)*((n%10000)/1000);
-              k=n;
-          }
-          else if(n>=1000){
-                n=(n/1000)*((n%1000)/100)*(n%10)*((n%100)/10);
-                k=n;
-            }
-            else if(n>=100){
-                n=(n/100)*((n%100)/10)*(n%10);
-                k=n;
-            }
-            else if(k>=10){
-                n=(n%10)*(n/10);
-                k=n;
-            }
-            j++;
-        }
-        return j;
-    }
+  }
+      return 1+ persistence(i);
 }
 int main(){
     int k =persistence(999);
