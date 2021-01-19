59e05644d07d6e9fa181bebc9ec9f974aa825b75
vmakar0v <ahmedmontassir0@outlook.com>
2021-01-19 13:52:53 +0100

    up

--- prob-in-C/test-land.c
+++ prob-in-C/test-land.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
-
-int main(){
-    int i,j,k;
+void ft_comb2(void){
+    int i,k,j,l;
     i = -1;
-    while(i++<8){
-      j=i;
-        while(j++<9){
-          k=j;
-            while(k++<9)
-            {
-                printf("%d",i);
-                printf("%d",j);
-                printf("%d",k);
-                if(i!=7){
-                    printf(", ");
+    while (i++<9){
+        k = -1;
+        while (k++<9){
+            j = -1;
+            while (j++<9){
+                l=-1;
+                while (l++<9){
+                    printf("%d",i);
+                    printf("%d",k);
+                    printf(" ");
+                    printf("%d",j);
+                    printf("%d",l);
+                    if(i!=9 || l!=9 || k!=9 || j!=9){
+                        printf(" ");
+                        printf(",");
+                    } 
                 }
             }
-        }
+        }       
     }
+}
+int main(){
+    ft_comb2();
     return 0;
 }
