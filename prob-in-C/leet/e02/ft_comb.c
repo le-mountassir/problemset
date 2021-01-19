9ffc85cca781ef97abdf6dca09d7db70cb1b822c
vmakar0v <ahmedmontassir0@outlook.com>
2021-01-19 01:09:15 +0100

    scc

--- prob-in-C/leet/e02/ft_comb.c
+++ prob-in-C/leet/e02/ft_comb.c
@@ -1,15 +1,28 @@
-#include <unistd.h>
-void ft_putchar(int j);
-void ft_prt_comb(void){
-	int i=-1;
-	while(i++<8){
-		ft_putchar(i);
-		while(k++<9){
-			ft_putchar(k);
-			j=k;
-			while(j++<=9){
-				ft_putchar(j);
-			}
-	}
-
+#include<unistd.h>
+void ft_putchar(char i){
+	write(1, &i, 1);
+}
+void ft_comb(void){
+    char i,j,k;
+    i='0'-1;
+    while(i++<'8'){
+        j=i;
+        while(j++<'9'){
+            k=j;
+            while(k++<'9')
+            {
+                ft_putchar(i);
+                ft_putchar(j);
+                ft_putchar(k);
+                if(i!='7'){
+                    ft_putchar(',');
+					ft_putchar(' ');
+                }
+            }
+        }
+    }
+}
+int main(){
+    ft_comb();
+    return 0;
 }
--- prob-in-C/test-land.c
+++ prob-in-C/test-land.c
@@ -2,12 +2,12 @@
 
 int main(){
     int i,j,k;
-    i=0;
-    while(i<8){
-        j=i+1;
-        while(j<9){
-            k=j+1;
-            while(k<=9)
+    i = -1;
+    while(i++<8){
+      j=i;
+        while(j++<9){
+          k=j;
+            while(k++<9)
             {
                 printf("%d",i);
                 printf("%d",j);
@@ -15,11 +15,8 @@ int main(){
                 if(i!=7){
                     printf(", ");
                 }
-                k++;
             }
-            j++;
         }
-        i++;
     }
     return 0;
 }
